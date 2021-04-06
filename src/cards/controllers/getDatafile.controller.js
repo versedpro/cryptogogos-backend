1e818886b8bfa1a84726e54659d7620130be16e3
umar-shafiq <umarshafiq2012@gmail.com>
2021-04-06 21:50:39 +0500

    addded fix

--- src/cards/controllers/getDatafile.controller.js
+++ src/cards/controllers/getDatafile.controller.js
@@ -1,35 +1,9 @@
 const fs = require('fs');
+
 const pinataSDK = require('@pinata/sdk');
 const pinata = pinataSDK('16c3984370294d4828c0', '3672043c01bc3257a644b78245ed973db78a7ea66cbe07bd3addc9995bcdd172');
-//Connceting to the ipfs network via infura gateway
-const IpfsHttpClient = require('ipfs-http-client')
-
-const ipfs = IpfsHttpClient()
 
 const getURIfile = async (id) => {
-    // return new Promise((resolve => {
-
-    //       fs.readFile(`src/public/meta/${id}.json`, async (err, data) => {
-    //         let ipfs_url;
-    //         if (err) {
-    //           console.log(err)
-
-    //         }
-    //         if (data) {
-    //           console.log('data', data)
-    //           const image = await ipfs.add(data);
-    //           console.log("image::::", image)
-    //           ipfs_url = `https://ipfs.io/ipfs/${image[0].hash}`;
-    //           console.log("Ipfs_url :::", ipfs_url);
-
-    //         }
-    //         return resolve(ipfs_url) 
-
-    //       })
-        
-    //   }))
-
-
     return new Promise(async (resolve, reject) => {
         const readableStreamForFile = fs.createReadStream(`src/public/meta/${id}.json`);
         // let metaFile = await fs.readFileSync(`src/public/meta/${id}.json`);
