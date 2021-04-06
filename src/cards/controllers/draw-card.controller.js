61804df975b5868f9d8f7497107776f4d223891e
umar-shafiq <umarshafiq2012@gmail.com>
2021-04-06 21:39:17 +0500

    fixed changes

--- src/cards/controllers/draw-card.controller.js
+++ src/cards/controllers/draw-card.controller.js
@@ -1,141 +1,147 @@
-const db = require("../../../models")
-const fs = require('fs');
-const path = require("path")
-const axios = require('axios');
+const db = require("../../../models");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
 const getURIfile = require("./getDatafile.controller");
 const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
 
 const drawCard = async (req, res, next) => {
-    try {
-        const cards = await db.cards.findAll({
-            where: {
-                released: true
-            }
-        });
-
-        let quantity = 1;
-        if (req.body.quantity) {
-            quantity = req.body.quantity
-        }
-        const itemDrps = new WeightedRandomBag();
-        const selectedCards = [];
-
-        for (let i = 0; i < cards.length; i++) {
-            itemDrps.addEntry(cards[i], cards[i].chance);
-        }
-        // let current = itemDrps.getRandom();  
-
-
-        // drawing random entries from it
-        for (let i = 0; i < quantity; i++) {
-            let current = itemDrps.getRandom();
-
-            if (current.current_supply === current.total_supply) {
-                i--;
-            } else {
-
-
-                let currentCard = await db.tokens.findOne({
-                    where: {
-                        serial_number: current.serial_number,
-                        type: current.type,
-                        supply_id: current.current_supply + 1
-                    },
-                });
-                if (currentCard) {
-                    if (currentCard.minted) {
-                        console.log('minted')
-                        i--;
-                        console.log('hit inside')
-                    } else {
-                        console.log('hit')
-                        let updateSupply = await db.cards.update({
-                            current_supply: current.current_supply + 1,
-                        }, {
-                            where: {
-                                id: current.id
-                            }
-                        })
-                        selectedCards.push(currentCard);
-                    }
-                }
-
-            }
-        }
-        const finalCards = [];
-        for (let cad of selectedCards) {
-
-            const obj = {
-
-                name: cad.name,
-                description: "",
-                image: cad.image,
-                attributes: [
-                    {
-                        trait_type: "Tribe",
-                        value: cad.tribe,
-                    },
-                    {
-                        trait_type: "Tier",
-                        value: cad.type,
-                    },
-                    {
-                        display_type: "number",
-                        trait_type: "Total Available",
-                        value: cad.supply_id,
-                        max_value: cad.total_supply
-                    },
-                    {
-                        display_type: "number",
-                        trait_type: "Serial No.",
-                        value: cad.supply_id,
-                        max_value: cad.total_supply
-                    }
-                ]
-            }
-
-            const json = JSON.stringify(obj, null, 4);
-            await fs.writeFile(
-                `src/public/meta/${cad.token_id}.json`,
-                json, (err) => {
-                    if (err) console.log(err)
-                }
-            );
-
-            const uadd = await getURIfile(cad.token_id);
-            const finalCard = {
-                card: cad,
-                image: uadd
-            }
-            finalCards.push(finalCard)
-        }
-        res.json(finalCards);
-
-    } catch (error) {
-        next(error)
+  try {
+    const cards = await db.cards.findAll({
+      where: {
+        released: true,
+      },
+    });
+    let quantity = 1;
+    if (req.body.quantity) {
+      quantity = req.body.quantity;
     }
+    const itemDrps = new WeightedRandomBag();
+    const selectedCards = [];
+    console.log('length', cards.length)
 
+    // for (let i = 0; i < cards.length; i++) {
+    //     itemDrps.addEntry(cards[i], cards[i].chance);
+    // }
+    // let current = itemDrps.getRandom();
 
-}
+    // drawing random entries from it
+    for (let i = 0; i < quantity; i++) {
+      let current = pick(cards);
+        // console.log('current', current)
+      if (current.current_supply === current.total_supply) {
+          console.log('hit')
+        i--;
+      } else {
+        let currentCard = await db.tokens.findOne({
+          where: {
+            serial_number: current.serial_number,
+            type: current.type,
+            supply_id: current.current_supply + 1,
+          },
+        });
+        if(currentCard === null) {
+            i--;
+        }
 
+        if (currentCard) {
+          if (currentCard.minted) {
+            console.log("minted");
+            i--;
+          } else {
+            let updateSupply = await db.cards.update(
+              {
+                current_supply: current.current_supply + 1,
+              },
+              {
+                where: {
+                  id: current.id,
+                },
+              }
+            );
+            selectedCards.push(currentCard);
+          }
+        }
+      }
+    }
+    const finalCards = [];
+    for (let cad of selectedCards) {
+      const obj = {
+        name: cad.name,
+        description: "",
+        image: cad.image,
+        attributes: [
+          {
+            trait_type: "Tribe",
+            value: cad.tribe,
+          },
+          {
+            trait_type: "Tier",
+            value: cad.type,
+          },
+          {
+            display_type: "number",
+            trait_type: "Total Available",
+            value: cad.supply_id,
+            max_value: cad.total_supply,
+          },
+          {
+            display_type: "number",
+            trait_type: "Serial No.",
+            value: cad.supply_id,
+            max_value: cad.total_supply,
+          },
+        ],
+      };
 
-const WeightedRandomBag = function () {
-    var entries = [];
-    var accumulatedWeight = 0.98;
-
-    this.addEntry = function (object, weight) {
-        accumulatedWeight += weight;
-        entries.push({ object: object, accumulatedWeight: accumulatedWeight });
-    };
-    this.entries = entries;
-    this.getRandom = function () {
-        var r = Math.random() * accumulatedWeight;
-        return entries.find(function (entry) {
-            return entry.accumulatedWeight >= r;
-        }).object;
-    };
+      const json = JSON.stringify(obj, null, 4);
+      await fs.writeFile(
+        `src/public/meta/${cad.token_id}.json`,
+        json,
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+      console.log("card", cad);
+       const uadd = await getURIfile(cad.token_id);
+      const finalCard = {
+        card: cad,
+        meta: uadd
+      };
+      finalCards.push(finalCard);
+    }
+    res.json(finalCards);
+  } catch (error) {
+    next(error);
+  }
 };
 
+const WeightedRandomBag = function () {
+  var entries = [];
+  var accumulatedWeight = 0.98;
 
+  this.addEntry = function (object, weight) {
+    accumulatedWeight += weight;
+    entries.push({ object: object, accumulatedWeight: accumulatedWeight });
+  };
+  this.entries = entries;
+  this.getRandom = function () {
+    var r = Math.random() * accumulatedWeight;
+    return entries.find(function (entry) {
+      return entry.accumulatedWeight >= r;
+    }).object;
+  };
+};
 
+const weight = (arr) => {
+  return [].concat(
+    ...arr.map((obj) => Array(Math.ceil(obj.chance * 100)).fill(obj))
+  );
+};
+const pick = (arr) => {
+    console.log(arr.length)
+  let weighted = weight(arr);
+  console.log(weighted.length)
+  return weighted[Math.floor(Math.random() * weighted.length)];
+};
 module.exports = drawCard;
--- src/cards/controllers/getDatafile.controller.js
+++ src/cards/controllers/getDatafile.controller.js
@@ -1,70 +1,68 @@
 const fs = require('fs');
-// const pinataSDK = require('@pinata/sdk');
-// const pinata = pinataSDK('16c3984370294d4828c0', '3672043c01bc3257a644b78245ed973db78a7ea66cbe07bd3addc9995bcdd172');
+const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK('16c3984370294d4828c0', '3672043c01bc3257a644b78245ed973db78a7ea66cbe07bd3addc9995bcdd172');
 //Connceting to the ipfs network via infura gateway
-const ipfsAPI = require('ipfs-api');
+const IpfsHttpClient = require('ipfs-http-client')
 
-const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' })
+const ipfs = IpfsHttpClient()
 
 const getURIfile = async (id) => {
-    return new Promise((resolve => {
+    // return new Promise((resolve => {
 
-          fs.readFile(`src/public/meta/${id}.json`, async (err, data) => {
-            let ipfs_url;
-            if (err) {
-              console.log(err)
+    //       fs.readFile(`src/public/meta/${id}.json`, async (err, data) => {
+    //         let ipfs_url;
+    //         if (err) {
+    //           console.log(err)
 
-            }
-            if (data) {
-              console.log('data', data)
-              const image = await ipfs.add(data);
-              console.log("image::::", image)
-              ipfs_url = `https://ipfs.io/ipfs/${image[0].hash}`;
-              console.log("Ipfs_url :::", ipfs_url);
+    //         }
+    //         if (data) {
+    //           console.log('data', data)
+    //           const image = await ipfs.add(data);
+    //           console.log("image::::", image)
+    //           ipfs_url = `https://ipfs.io/ipfs/${image[0].hash}`;
+    //           console.log("Ipfs_url :::", ipfs_url);
 
-            }
-            return resolve(ipfs_url) 
+    //         }
+    //         return resolve(ipfs_url) 
 
-          })
+    //       })
         
-      }))
+    //   }))
 
 
-    // return new Promise(async (resolve, reject) => {
-    //     // const readableStreamForFile = fs.createReadStream(`src/public/meta/${id}.json`);
-    //     let metaFile = await fs.readFileSync(`src/public/meta/${id}.json`);
-    //     console.log('metaFile', metaFile)
-    //     let metaBuffer = new Buffer.from(metaFile);
-    //     console.log('metaFile', metaBuffer)
+    return new Promise(async (resolve, reject) => {
+        const readableStreamForFile = fs.createReadStream(`src/public/meta/${id}.json`);
+        // let metaFile = await fs.readFileSync(`src/public/meta/${id}.json`);
+        // console.log('metaFile', metaFile)
+        // let metaBuffer = new Buffer.from(metaFile);
 
-    //     // const options = {
-    //     //     pinataMetadata: {
-    //     //         name: JSON.stringify(id),
-    //     //     },
-    //     //     pinataOptions: {
-    //     //         cidVersion: 0
-    //     //     }
-    //     // };
-    //     try {
-    //         // const file = await pinata.pinFileToIPFS(readableStreamForFile, options);
-    //         // console.log('file', file)
+        const options = {
+            pinataMetadata: {
+                name: JSON.stringify(id),
+            },
+            pinataOptions: {
+                cidVersion: 0
+            }
+        };
+        try {
+            const file = await pinata.pinFileToIPFS(readableStreamForFile, options);
+            console.log('file', file)
 
-    //         await ipfs.files.add(metaBuffer, (err, file) => {
-    //             if (err) {
-    //                 console.log(err);
+            await ipfs.files.add(metaBuffer, (err, file) => {
+                if (err) {
+                    console.log(err);
 
-    //             }
-    //             // console.log('file', file)
-    //             resolve(file)
+                }
+                resolve(file)
 
-    //             console.log(file)
-    //         })
-    //         resolve(file)
-    //     } catch (error) {
-    //         reject(error)
-    //     }
+                console.log(file)
+            })
+            resolve(file)
+        } catch (error) {
+            reject(error)
+        }
 
-    // })
+    })
 
 }
 
