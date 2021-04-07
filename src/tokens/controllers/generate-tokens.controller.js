23bb37c50f0cdcdc454a791b4299d8b539e37381
Hossam Saraya <hossam.saraya@gmail.com>
2021-04-07 04:38:15 +0200

    deploytyy

--- src/tokens/controllers/generate-tokens.controller.js
+++ src/tokens/controllers/generate-tokens.controller.js
@@ -2,30 +2,30 @@ const db = require('../../../models');
 const dataArr = require('../../data/tokens_data.json')
 const generateTokens =  async (req, res, next) => {
 
-    console.log("gen tokens")
-try {
+//     console.log("gen tokens")
+// try {
 
-    if(dataArr) {
-        for(let token of dataArr){
+//     if(dataArr) {
+//         for(let token of dataArr){
             
-            token.serial_number = +token.serial_number;
-            token.current_supply = 0
-            token.total_supply = +token["total supply"];
-            token.serial_number = +token.serial_number;
+//             token.serial_number = +token.serial_number;
+//             token.current_supply = 0
+//             token.total_supply = +token["total supply"];
+//             token.serial_number = +token.serial_number;
 
-            const dbToken = await db.tokens.create(token);
-        }
-    }
+//             const dbToken = await db.tokens.create(token);
+//         }
+//     }
 
 
 
-    const tokens = await db.tokens.findAll();
-    res.json(tokens).status(200)
+//     const tokens = await db.tokens.findAll();
+//     res.json(tokens).status(200)
     
-} catch (error) {
-    console.error(error)
-    next(error)
-}
+// } catch (error) {
+//     console.error(error)
+//     next(error)
+// }
 
 }
 
