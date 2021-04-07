0eb483dcd4675676ba6503c99c8b60248c88f64c
Hossam Saraya <hossam.saraya@gmail.com>
2021-04-07 21:29:40 +0200

    dev and prod envs with automatic envloading

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+const DEFAULT_ENV = 'development';
+const dotenv = require('dotenv')
+dotenv.config({path: `${__dirname}/../config/.env.${process.env.NODE_ENV || DEFAULT_ENV}`})
+
+console.log(process.env.INFURA_RPC_URL)
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
